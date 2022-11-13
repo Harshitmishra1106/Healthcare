e2078427ffe1502be2950f2dc234c083b8247de3
HARSH <theharsh1210@gmail.com>
2022-11-14 01:15:42 +0530

    dashboard

--- app/src/main/java/com/example/healthcare/Dashboard.java
+++ app/src/main/java/com/example/healthcare/Dashboard.java
@@ -22,10 +22,12 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Temperature");
-        DatabaseReference myRef1 = database.getReference("Humidity");
+        DatabaseReference myRef1 = database.getReference("Pulse");
+        DatabaseReference myRef2 = database.getReference("Oxygen");
 
         final TextView temp = findViewById(R.id.temperature);
-        final TextView humdity = findViewById(R.id.humidity);
+        final TextView pul = findViewById(R.id.pulse);
+        final TextView oxy = findViewById(R.id.oxygen);
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -48,7 +50,22 @@ public class Dashboard extends AppCompatActivity {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 String value = dataSnapshot.getValue().toString();
-                humdity.setText(value+" %");
+                pul.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue().toString();
+                oxy.setText(value);
             }
 
             @Override
